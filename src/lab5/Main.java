95aded9d04d36db1c36616037d485eb77b5170d1
makszaranik <52628168+makszaranik@users.noreply.github.com>
2024-03-27 09:18:51 +0200

    add Main and RarestWordFinder

--- src/lab5/Main.java
+++ src/lab5/Main.java
@@ -1,2 +1,25 @@
-package lab5;public class main {
+package lab5;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String res = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader("files/input"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                res = res + line + " ";
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("IOException during file reading: " + e.getMessage());
+        }
+
+        RarestWordFinder rarestWordFinder = new RarestWordFinder(res);
+        String rarestWord = rarestWordFinder.findRarestWord();
+
+        System.out.println("rarestWord = " + rarestWord);
+
+    }
 }
